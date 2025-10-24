afe90628c52b0440361249c56d2d863a824f6e7d
Nick <melancholic8078@gmail.com>
2025-10-24 14:41:03 +0300

    changed type for port and added decorator for optimization

--- app/core/config.py
+++ app/core/config.py
@@ -3,14 +3,14 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
     host: str="0.0.0.0"
-    port: str="8000"
+    port: int="8000"
     origins: list[str]=["http://localhost:3000"]
 
     class Config:
         env_file=".env"
         env_file_encoding="utf-8"
         
-        
+@lru_cache    
 def get_settings():
     return Settings()
         
