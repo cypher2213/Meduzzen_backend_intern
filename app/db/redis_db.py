87ac22d425c5602fe304213a9e881049d545f542
Nick <melancholic8078@gmail.com>
2025-10-25 21:01:32 +0300

    used with construction so it is automatically close connection and pool

--- app/db/redis_db.py
+++ app/db/redis_db.py
@@ -4,17 +4,14 @@ import asyncio
 REDIS_URL = settings.REDIS_URL
 
 async def redis_main():
-    redis_connect = redis.from_url(REDIS_URL,decode_responses=True)
-
-    try:
-        redis_answer = await redis_connect.ping()
-        if(redis_answer):
-            print("Redis connected successfully")
-    except Exception as e:
-        print("Failed to connect to the Redis =>", e)
-    finally:
-        await redis_connect.aclose()
-        await redis_connect.connection_pool.disconnect()
+    async with redis.from_url(REDIS_URL,decode_responses=True) as redis_connect:
+        try:
+            answer = await redis_connect.ping()
+            if answer:
+                print("Redis successfully connected with async")
+        except Exception as e:
+            print("Failed to connect to the Redis =>", e)
+            
         
     
     
