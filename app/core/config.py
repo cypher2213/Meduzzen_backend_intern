699bdd46f6c56eb16d9ddf73e3d8c3d91b5fed40
Nick <melancholic8078@gmail.com>
2025-10-25 20:52:42 +0300

    added redis url

--- app/core/config.py
+++ app/core/config.py
@@ -4,8 +4,9 @@ from functools import lru_cache
 class Settings(BaseSettings):
     HOST: str = "0.0.0.0"
     PORT: int = 8000
-    ORIGINS: list[str] =["http://localhost:3000"]
-
+    ORIGINS: list[str] = ["http://localhost:3000"]
+    DATABASE_URL: str
+    REDIS_URL: str = "redis://localhost:6379/0"
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
