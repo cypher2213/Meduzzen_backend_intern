bb8b9f59d398be7f2b5ced6d89f9cd1ed8a5bf55
Nick <melancholic8078@gmail.com>
2025-10-26 00:11:46 +0300

    added new variables due to redis and psql addition

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,11 @@ class Settings(BaseSettings):
     PORT: int = 8000
     ORIGINS: list[str] = ["http://localhost:3000"]
     DATABASE_URL: str
-    REDIS_URL: str = "redis://localhost:6379/0"
+    REDIS_URL: str
+    REDIS_HOST: str
+    POSTGRES_USER:str
+    POSTGRES_DB:str
+    DOCKER_DATABASE_URL:str
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
