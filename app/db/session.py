7dbceb4bb2a82421877c21408f4db5a8e2b32eff
Nick <melancholic8078@gmail.com>
2025-10-26 00:12:30 +0300

    psql connection file

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,6 @@
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-import asyncio
 from app.core.config import settings
 
 DATABASE_URL = settings.DATABASE_URL
@@ -15,11 +14,10 @@ AsyncSessionLocal = sessionmaker(
     expire_on_commit=False 
 )
 
-async def connection_test():
+async def posgtres_main():
     async with AsyncSessionLocal() as session:
         result = await session.execute(text("SELECT 1"))
         value = result.scalar()
         print("Postgres connected successfully:",value)
-
-asyncio.run(connection_test())
+        return True if value else False
 
