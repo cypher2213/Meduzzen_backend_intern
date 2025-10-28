05aa2d4b923226af411dd4948245e4b68569c7bd
Nick <melancholic8078@gmail.com>
2025-10-28 02:27:16 +0200

    updated config

--- app/core/config.py
+++ app/core/config.py
@@ -5,12 +5,6 @@ class Settings(BaseSettings):
     HOST: str = "0.0.0.0"
     PORT: int = 8000
     ORIGINS: list[str] = ["http://localhost:3000"]
-    DATABASE_URL: str
-    REDIS_URL: str
-    REDIS_HOST: str
-    POSTGRES_USER: str
-    POSTGRES_DB: str
-    DOCKER_DATABASE_URL: str
 
     class Config:
         env_file = ".env"
