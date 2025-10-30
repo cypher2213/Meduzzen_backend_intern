1dc97a181b716652df6543dd7f5e709675ae9609
Nick <melancholic8078@gmail.com>
2025-10-31 01:49:43 +0200

    added delete user route

--- app/routers/users.py
+++ app/routers/users.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.session import get_session
 from app.schemas.user_schema import SignUpSchema, UserSchema
-from app.services.users_service import user_serice
+from app.services.users_service import user_service
 
 router = APIRouter()
 
@@ -14,10 +14,15 @@ router = APIRouter()
 async def get_users(
     limit: int = 10, offset: int = 0, session: AsyncSession = Depends(get_session)
 ):
-    users = await user_serice.get_all_users(session, limit, offset)
+    users = await user_service.get_all_users(session, limit, offset)
     return users
 
 
 @router.post("/", response_model=UserSchema)
 async def user_create(user: SignUpSchema, session: AsyncSession = Depends(get_session)):
-    return await user_serice.create_user(session, user.model_dump())
+    return await user_service.create_user(session, user.model_dump())
+
+
+@router.delete("/{user_id}")
+async def user_delete(user_id: int, session: AsyncSession = Depends(get_session)):
+    return await user_service.delete_user(session, user_id)
