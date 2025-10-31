9f8b332be138527f34a4b14e1ad01018de64b788
Nick <melancholic8078@gmail.com>
2025-10-31 20:53:43 +0200

    logger added

--- app/services/health_service.py
+++ app/services/health_service.py
@@ -2,6 +2,7 @@ import redis.asyncio as redis
 from sqlalchemy import text
 
 from app.core.config import settings
+from app.core.logger import logger
 from app.db.session import AsyncSessionLocal
 
 REDIS_URL = settings.redis.REDIS_URL
@@ -12,10 +13,10 @@ async def redis_main():
         try:
             answer = await redis_connect.ping()
             if answer:
-                print("Redis successfully connected with async")
+                logger.info("Redis successfully connected with async")
                 return True
         except Exception as e:
-            print("Failed to connect to the Redis =>", e)
+            logger.info("Failed to connect to the Redis =>", e)
             return False
 
 
@@ -23,5 +24,5 @@ async def posgtres_main():
     async with AsyncSessionLocal() as session:
         result = await session.execute(text("SELECT 1"))
         value = result.scalar()
-        print("Postgres connected successfully:", value)
+        logger.info("Postgres connected successfully:")
         return True if value else False
