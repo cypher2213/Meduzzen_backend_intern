8c339f0c6dcc4629cbf2c180b7531015740a2a7b
Nick <melancholic8078@gmail.com>
2025-10-31 23:38:15 +0200

    cleared db/session

--- app/db/session.py
+++ app/db/session.py
@@ -3,9 +3,8 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-DATABASE_URL = settings.db.url
 engine = create_async_engine(
-    DATABASE_URL,
+    settings.db.url,
 )
 
 AsyncSessionLocal = sessionmaker(
