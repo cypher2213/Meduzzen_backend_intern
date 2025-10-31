5127fa7edc251f10aec96131788c9e21d4c54f20
Nick <melancholic8078@gmail.com>
2025-10-31 20:54:32 +0200

    changed database_url

--- app/db/session.py
+++ app/db/session.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-DATABASE_URL = f"postgresql+asyncpg://{settings.db.POSTGRES_USER}:@{settings.db.POSTGRES_HOST}:{settings.db.POSTGRES_PORT}/{settings.db.POSTGRES_DB}"
+DATABASE_URL = settings.db.url
 engine = create_async_engine(
     DATABASE_URL,
 )
