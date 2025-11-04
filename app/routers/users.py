cbe6c80bd85ffcfd59085da40ded8ad4095ae983
Nick <melancholic8078@gmail.com>
2025-11-05 00:57:42 +0200

    added redis as dependency

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,9 +1,10 @@
 from typing import List
 
 from fastapi import APIRouter, Depends
+from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.db.session import get_session
+from app.db.session import get_redis, get_session
 from app.schemas.user_schema import SignUpSchema, UserSchema, UserUpdateSchema
 from app.services.users_service import user_service
 
@@ -12,29 +13,47 @@ router = APIRouter()
 
 @router.get("/", response_model=List[UserSchema])
 async def get_users(
-    limit: int = 10, offset: int = 0, session: AsyncSession = Depends(get_session)
+    limit: int = 10,
+    offset: int = 0,
+    session: AsyncSession = Depends(get_session),
+    redis: Redis = Depends(get_redis),
 ):
-    users = await user_service.get_all_users(session, limit, offset)
+    users = await user_service.get_all_users(session, redis, limit, offset)
     return users
 
 
 @router.post("/", response_model=UserSchema)
-async def user_create(user: SignUpSchema, session: AsyncSession = Depends(get_session)):
-    return await user_service.create_user(session, user.model_dump())
+async def user_create(
+    user: SignUpSchema,
+    session: AsyncSession = Depends(get_session),
+    redis: Redis = Depends(get_redis),
+):
+    return await user_service.create_user(session, user.model_dump(), redis)
 
 
 @router.delete("/{user_id}")
-async def user_delete(user_id: int, session: AsyncSession = Depends(get_session)):
-    return await user_service.delete_user(session, user_id)
+async def user_delete(
+    user_id: int,
+    session: AsyncSession = Depends(get_session),
+    redis: Redis = Depends(get_redis),
+):
+    return await user_service.delete_user(session, user_id, redis)
 
 
 @router.get("/{user_id}")
-async def user_by_id(user_id: int, session: AsyncSession = Depends(get_session)):
-    return await user_service.get_user_by_id(session, user_id)
+async def user_by_id(
+    user_id: int,
+    session: AsyncSession = Depends(get_session),
+    redis: Redis = Depends(get_redis),
+):
+    return await user_service.get_user_by_id(session, user_id, redis)
 
 
 @router.patch("/{user_id}")
 async def user_update(
-    user_id: int, user: UserUpdateSchema, session: AsyncSession = Depends(get_session)
+    user_id: int,
+    user: UserUpdateSchema,
+    session: AsyncSession = Depends(get_session),
+    redis: Redis = Depends(get_redis),
 ):
-    return await user_service.update_user(user_id, user.model_dump(), session)
+    return await user_service.update_user(user_id, user.model_dump(), session, redis)
