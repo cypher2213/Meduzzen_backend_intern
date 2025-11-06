5c22d0a667d392c25b2d0df7f05bcd361247a756
Nick <melancholic8078@gmail.com>
2025-11-06 12:23:53 +0200

    mixin separated

--- app/models/timestamp_mixin.py
+++ app/models/timestamp_mixin.py
@@ -1,16 +1,10 @@
-import uuid
 from typing import Optional
-from uuid import UUID as PyUUID
 
 from sqlalchemy import DateTime, func
-from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
 
-class TimestampUUIDMixin:
-    id: Mapped[PyUUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
-    )
+class TimestampMixin:
     created_at: Mapped[Optional[DateTime]] = mapped_column(
         DateTime(timezone=True), server_default=func.now()
     )
