a60e3c262ba649eb43882c2cfe53ff95a637d867
Nick <melancholic8078@gmail.com>
2025-11-07 13:09:10 +0200

    changed from dict to schemas for better validation

--- app/services/users_service.py
+++ app/services/users_service.py
@@ -1,14 +1,12 @@
-import json
 from uuid import UUID
 
 from fastapi import HTTPException
 from pwdlib import PasswordHash
-from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.logger import logger
-from app.db.users_repository import UserRepository
-from app.schemas.user_schema import UserSchema
+from app.repository.users_repository import UserRepository
+from app.schemas.user_schema import SignUpSchema, UserSchema, UserUpdateSchema
 
 pwd_context = PasswordHash.recommended()
 
@@ -18,70 +16,46 @@ class UserService:
         self.repo = repo
 
     async def get_all_users(
-        self, session: AsyncSession, redis: Redis, limit: int = 10, offset: int = 0
+        self, session: AsyncSession, limit: int = 10, offset: int = 0
     ):
-        cache_key = f"users:{limit}:{offset}"
-        cached_data = await redis.get(cache_key)
-        if cached_data:
-            logger.info("Users fetched from redis cache")
-            return json.loads(cached_data)
         users = await self.repo.get_all(session, limit, offset)
         if users:
             user_list = [
                 UserSchema.model_validate(user).model_dump(mode="json")
                 for user in users
             ]
-            await redis.set(cache_key, json.dumps(user_list), ex=300)
-            logger.info("Users cached in redis")
-        return users or []
+        return user_list or []
 
-    async def create_user(self, session: AsyncSession, user_data: dict, redis: Redis):
+    async def create_user(self, session: AsyncSession, user_data: SignUpSchema):
         if "password" in user_data:
             user_data["password"] = pwd_context.hash(user_data["password"])
         user = await self.repo.create(session, user_data)
-        await redis.delete(f"user:{user.id}")
-        async for key in redis.scan_iter("users:*"):
-            await redis.delete(key)
         logger.info(f"User created: id={user.id}, name={user.name}")
         return user
 
-    async def delete_user(self, session: AsyncSession, user_id: UUID, redis: Redis):
+    async def delete_user(self, session: AsyncSession, user_id: UUID):
         user = await self.repo.get_by_id(session, user_id)
         if not user:
             logger.warning(f"Attempted delete — user not found: id={user_id}")
             raise HTTPException(status_code=404, detail="User not found")
         await self.repo.delete(session, user)
-        await redis.delete(f"user:{user_id}")
-        async for key in redis.scan_iter("users:*"):
-            await redis.delete(key)
         logger.info(f"User deleted: id={user_id}, name={user.name}")
         return {"message": f"User with name {user.name} successfully deleted!"}
 
-    async def get_user_by_id(self, session: AsyncSession, user_id: UUID, redis: Redis):
-        cache_key = f"user:{user_id}"
-        cached_data = await redis.get(cache_key)
-        if cached_data:
-            logger.info("User fetched from redis cache")
-            return json.loads(cached_data)
+    async def get_user_by_id(self, session: AsyncSession, user_id: UUID):
         user = await self.repo.get_by_id(session, user_id)
         if not user:
             raise HTTPException(status_code=404, detail="User not found")
-        user_dict = UserSchema.model_validate(user).model_dump(mode="json")
-        await redis.set(cache_key, json.dumps(user_dict), ex=300)
-        logger.info("User cached in redis")
         return {"message": "User Found", "user": user}
 
     async def update_user(
-        self, user_id: UUID, updated_user: dict, session: AsyncSession, redis: Redis
+        self, user_id: UUID, updated_user: UserUpdateSchema, session: AsyncSession
     ):
         user = await self.repo.get_by_id(session, user_id)
         if not user:
             raise HTTPException(status_code=404, detail="User not found")
         filtered_data = {k: v for k, v in updated_user.items() if v is not None}
         updated_user_obj = await self.repo.update(session, user, filtered_data)
-        await redis.delete(f"user:{user_id}")
-        async for key in redis.scan_iter("users:*"):
-            await redis.delete(key)
         logger.info(f"User updated: id={user.id}")
         return {
             "message": "User updated successfully",
