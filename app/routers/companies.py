e6e5d251113fa40cf26a9ac92a209de8d93dd95a
Nick <melancholic8078@gmail.com>
2025-11-14 02:50:04 +0200

    added routes for creating a company

---
+++ app/routers/companies.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.session import get_session
+from app.models.user_model import UserModel
+from app.schemas.company_schema import CompanyCreate
+from app.services.companies_service import companies_service
+from app.utils.user_util import user_connect
+
+router = APIRouter()
+
+
+@router.post("/")
+async def create_company(
+    company: CompanyCreate,
+    session: AsyncSession = Depends(get_session),
+    current_user: UserModel = Depends(user_connect),
+):
+    return await companies_service.company_create(company, session, current_user)
