deeaa6068160f506fa79ffdbc41f526a1e24f364
Nick <melancholic8078@gmail.com>
2025-11-14 03:26:14 +0200

    added company update route

--- app/routers/companies.py
+++ app/routers/companies.py
@@ -1,9 +1,11 @@
+from uuid import UUID
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.session import get_session
 from app.models.user_model import UserModel
-from app.schemas.company_schema import CompanyCreate
+from app.schemas.company_schema import CompanyCreate, CompanyUpdate
 from app.services.companies_service import companies_service
 from app.utils.user_util import user_connect
 
@@ -17,3 +19,15 @@ async def create_company(
     current_user: UserModel = Depends(user_connect),
 ):
     return await companies_service.company_create(company, session, current_user)
+
+
+@router.patch("/{company_id}")
+async def update_company(
+    company_id: UUID,
+    company: CompanyUpdate,
+    session: AsyncSession = Depends(get_session),
+    current_user: UserModel = Depends(user_connect),
+):
+    return await companies_service.company_update(
+        company_id, company, session, current_user
+    )
