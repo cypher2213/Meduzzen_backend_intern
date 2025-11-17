7a8930af4c0afaf7744df2ddac1e8cc01d32939e
Nick <melancholic8078@gmail.com>
2025-11-18 01:33:43 +0200

    updated company services

--- app/services/companies_service.py
+++ app/services/companies_service.py
@@ -25,7 +25,7 @@ class CompaniesService:
         return company_list or []
 
     async def get_company(self, company_id: UUID, session: AsyncSession):
-        company = await self.repo.company_get(company_id, session)
+        company = await self.repo.get(session, company_id)
         if not company:
             raise HTTPException(
                 status_code=404,
@@ -36,12 +36,7 @@ class CompaniesService:
     async def company_create(
         self, company_data: CompanyCreate, db: AsyncSession, user: UserModel
     ):
-        new_company = await self.repo.create_company(
-            db=db,
-            name=company_data.name,
-            description=company_data.description,
-            is_public=company_data.is_public,
-        )
+        new_company = await self.repo.create(db, data=company_data.model_dump())
         await self.repo.add_user_role(
             db=db, user_id=user.id, company_id=new_company.id, role=RoleEnum.OWNER
         )
@@ -67,7 +62,7 @@ class CompaniesService:
         for field, value in company_data.model_dump(exclude_unset=True).items():
             setattr(company, field, value)
 
-        updated_company = await self.repo.update_company(db, company)
+        updated_company = await self.repo.update(db, company)
 
         return {
             "message": f"Company {updated_company.name} updated successfully.",
@@ -75,12 +70,13 @@ class CompaniesService:
         }
 
     async def company_delete(self, company_id: UUID, db: AsyncSession, user: UserModel):
-        company = await self.repo.delete_company(db, company_id, user)
+        company = await self.repo.get_owner_company(db, company_id, user.id)
         if not company:
             raise HTTPException(
                 status_code=403,
                 detail="You are not the owner of this company or it does not exist.",
             )
+        await self.repo.delete(db, company)
 
 
 companies_service = CompaniesService(CompaniesRepository())
