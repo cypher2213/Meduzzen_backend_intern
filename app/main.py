19b3fc4264ba4fe8f7fc31eef20fceab35715610
Nick <melancholic8078@gmail.com>
2025-11-21 21:13:51 +0200

    main py updated for custom exceptions

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
+from app.core.base_exception import BaseServiceError
 from app.core.config import settings
 from app.core.error_middleware import error_middleware
 from app.routers.route_collection import router as api_routes
 
 app = FastAPI()
+
+
+@app.exception_handler(BaseServiceError)
+async def base_service_error_handler(request, exc: BaseServiceError):
+    return JSONResponse(status_code=exc.status_code, content={"detail": exc.message})
+
 
 app.middleware("http")(error_middleware)
 app.add_middleware(
